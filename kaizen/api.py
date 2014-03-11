afbe83523f6c71bcdc21ffc14d5d45064030637d
bvidal <bvidal@ltutech.com>
2014-03-11 00:06:05 -0400

    Make it possible to retrieve your project(s)
    The almighty ZenRequest has arrived to save the day.
    
    Trying to make it easy to retrieve resources by simply
    'constructing' the request you can now retrieve your
    project(s) as easily as:
    
    ZenRequest(api_key).projects(project_id).send()

---
+++ kaizen/api.py
@@ -0,0 +1,43 @@
+from kaizen.client import ApiClient
+from kaizen.request import Verbs, Request
+
+
+def _default(arg):
+  """Return the empty string if the arg is None"""
+  return (arg if arg is not None else "")
+
+
+class ZenRequest(Request):
+  """The main object to use to retrieve resources from the AgileZen API.
+
+  Getting information on one of your projects is as simply as:
+  ZenRequest(api_key).projects(project_id).send()
+  """
+  # TODO(bvidal): the method send could cache the result from the API
+  # and invalid it if any other method is called. Not sure it's useful
+  # though... So I don't need it for now.
+
+  def __init__(self, api_key):
+    Request.__init__(self)
+    self._client = ApiClient(api_key)
+
+  def send(self):
+    """Send the request to the API.
+
+    Returns:
+      - the JSON dict response from AgileZen
+    Raises:
+      - requests.exceptions.HTTPError if the request is not successful
+    """
+    return self._client.make_request(self.verb, self.url, self.params,
+                                     self.data)
+
+  def projects(self, project_id=None):
+    """Access the Project resource. If project_id is None the request will list
+    the Project you have access to.
+
+    Args:
+      project_id: id of a specific Project, defaults to None
+    """
+    return self.update_url("/projects/%s" % _default(project_id))
+
