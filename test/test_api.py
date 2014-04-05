cce797af3d28f311f11f582f6a517e3c8cd4edea
bvidal <bvidal@ltutech.com>
2014-04-05 11:18:06 -0400

    Use 4 spaces instead of 2 in python files
    For multiple reason:
    - I usually work with 2 and wanted to try 4
    - Landscape doesn't offer the possibility to configure that yet

--- test/test_api.py
+++ test/test_api.py
@@ -4,10 +4,10 @@ from kaizen.api import ZenRequest, ChainingError
 
 class ZenRequestTest(unittest.TestCase):
 
-  def testChainingProjectPhase(self):
-    request = ZenRequest("fake_key")
-    self.assertRaises(ChainingError, request.phases)
+    def testChainingProjectPhase(self):
+        request = ZenRequest("fake_key")
+        self.assertRaises(ChainingError, request.phases)
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
