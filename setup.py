3609eaeeb2e118fd4fd24aed24b31a5095a335c9
bertrandvidal <vidal.bertrand@gmail.com>
2015-02-28 21:45:30 +0100

    Fix setup.py coding style
    Line too long
    Space around '=' in method call
    
    Tests:

--- setup.py
+++ setup.py
@@ -2,31 +2,33 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst'), "r") as readme_file:
-    readme = readme_file.read()
+README_PATH = os.path.join(os.path.dirname(__file__), 'README.rst')
+with open(README_PATH, "r") as readme_file:
+    README = readme_file.read()
 
 setup(
-    name = "kaizen",
-    version = "0.1.4",
-    description = "A python client and cli to manage your projects on AgileZen Kanban style.",
-    long_description = readme,
-    packages = ["kaizen"],
-    author = "Bertrand Vidal",
-    author_email = "vidal.bertrand@gmail.com",
-    download_url = "https://pypi.python.org/pypi/kaizen",
-    url = "https://github.com/bertrandvidal/kaizen",
-    classifiers = [
+    name="kaizen",
+    version="0.1.4",
+    description=("A python client and cli to manage your projects on AgileZen"
+                 " Kanban style."),
+    long_description=README,
+    packages=["kaizen"],
+    author="Bertrand Vidal",
+    author_email="vidal.bertrand@gmail.com",
+    download_url="https://pypi.python.org/pypi/kaizen",
+    url="https://github.com/bertrandvidal/kaizen",
+    classifiers=[
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
     ],
-    setup_requires = [
+    setup_requires=[
         "nose",
         "responses",
     ],
-    install_requires = [
+    install_requires=[
         "requests",
     ],
 )
