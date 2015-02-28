da79e3aef616e08263dca9f6feaea6edbf80d924
bertrandvidal <vidal.bertrand@gmail.com>
2015-02-28 21:40:00 +0100

    Fix test names

--- test/test_request.py
+++ test/test_request.py
@@ -5,13 +5,13 @@ from kaizen.request import VERBS, Request
 
 class VerbsTest(unittest.TestCase):
 
-    def testIn(self):
+    def test_http_verbs_in(self):
         self.assertIn("GET", VERBS)
         self.assertIn("POST", VERBS)
         self.assertIn("PUT", VERBS)
         self.assertIn("DELETE", VERBS)
 
-    def testNotIn(self):
+    def test_http_verbs_not_in(self):
         self.assertNotIn("NoWay", VERBS)
         self.assertNotIn("OPTIONS", VERBS)
         self.assertNotIn("HEAD", VERBS)
@@ -22,14 +22,14 @@ class RequestTest(unittest.TestCase):
     def setUp(self):
         self._request = Request()
 
-    def testSetVerb(self):
+    def test_set_verb(self):
         def assign_verb(new_verb):
             """To make it possible to use assertRaises."""
             self._request.verb = new_verb
         self.assertRaises(ValueError, assign_verb, "Nop!")
         self.assertRaises(ValueError, self._request.update_verb, "Nop!")
 
-    def testCallChaining(self):
+    def test_call_chaining(self):
         post_request = self._request.update_verb("POST").update_url("/call_me")\
             .update_params({"name": "popo"}).update_data({"text": "cool beans"})
         self.assertEquals(post_request, self._request)
