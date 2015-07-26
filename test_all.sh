f113fa48c50a7fb35ddf1c239abab701209381d4
bertrandvidal <vidal.bertrand@gmail.com>
2015-07-26 19:18:23 +0200

    Update venv prompt created by test_all script
    Forgot to update the virtualenv prompt when copied the script from
    my parse_this github repo.

--- test_all.sh
+++ test_all.sh
@@ -5,7 +5,7 @@ function test_python_version(){
   ENV_PATH=$1
   PY_VERSION=$2
   if [ ! -d ${ENV_PATH} ]; then
-    virtualenv --prompt="(parse_this_${PY_VERSION})" --python=python${PY_VERSION} ${ENV_PATH}
+    virtualenv --prompt="(kaizen_${PY_VERSION})" --python=python${PY_VERSION} ${ENV_PATH}
   fi
   source ${ENV_PATH}/bin/activate
   python setup.py develop
